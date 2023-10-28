39c754ff668a8955df62e5fde2f2bc05253ed696
Taras Kovalyk <tarik2454@gmail.com>
2023-10-28 22:01:13 +0300

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    base: '/vite-project-template/',
     rollupOptions: {
       input: './src/main.jsx',
     },
