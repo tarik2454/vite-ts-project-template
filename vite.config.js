017f2a10dfb5a87f050be0047b3e25628553963d
Taras Kovalyk <tarik2454@gmail.com>
2023-10-28 21:24:29 +0300

    fix bug deploy

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  root: 'src',
   build: {
     rollupOptions: {
-      input: 'src/main.jsx',
+      input: './src/main.jsx',
     },
-    outDir: '../dist',
+    outDir: './dist',
   },
-  base: '/',
+  plugins: [react()],
 });
