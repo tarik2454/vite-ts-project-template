765410e2873894bab50efda2d38d76062948faaa
Taras Kovalyk <tarik2454@gmail.com>
2024-01-10 17:50:06 +0200

    adds changes template

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   base: '/vite-project-template/',
 });
